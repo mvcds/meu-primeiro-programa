64780ac858163f0dc1bb3836053905d54975e862
Marcos Vinícius C. da Silva <marcos.silva@sprinklr.com>
2018-04-21 17:28:35 -0300

    completa o programa básico

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-function dizerOla ({ nome, caze, upper }) {
-  console.log(nome, caze, upper)
+function aplicarCaixa (nome, isUpperCase) {
+  if (isUpperCase) return nome.toUpperCase()
+
+  return nome
+}
+
+function aplicarCase (nome, caze) {
+  switch (caze) {
+    case 'kebab': return nome.replace(' ', '-')
+  }
+
+  return nome
+}
+
+function dizerOla ({ nome = 'world', caze, upper = false }) {
+  const nomeCaixiado = aplicarCaixa(nome, upper)
+
+  const nomeCasado = aplicarCase(nomeCaixiado, caze)
+
+  console.log('Hello', nomeCasado)
 }
 
 const meuPrograma = {
